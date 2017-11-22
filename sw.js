5a319dba06dfd6d28a8cd877976d639f8a0dbe94
cedricrettus <cedric.sutter@ntb.ch>
2017-11-22 18:01:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-
-self.addEventListener('fetch', function(event) {
-  if (/\.jpg$/.test(event.request.url)) {
-    event.respondWith(fetch('cat.jpg'));
-  }
+self.addEventListener('install', function(event) {
+  // Perform install steps
 });
+
+
+self.addEventListener('fetch', function(event) { 
+	if (/\.jpg$/.test(event.request.url)) { 
+		event.respondWith(fetch('unicorn.jpg’)); 
+	}
+});
+
